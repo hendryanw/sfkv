364ff149a6d4c24fa1b8c18594c33691b79643fa
Hendry Anwar <hendry.anwar@bertelsmann.de>
2018-02-23 14:31:54 +0800

    Added ZuraZura code sample. - Creating a new sample of matchmaking controller in ZuraZura.MatchmakingService. The sample use the SFKV as a temporary storage of members participating in matchmaking.

---
+++ Examples/ZuraZura/ZuraZura.MatchmakingService/Controllers/MatchmakingController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Newtonsoft.Json;
+using SFKV.Contracts;
+
+namespace ZuraZura.MatchmakingService.Controllers
+{
+    /// <summary>
+    /// The matchmaking members class contains the name of each participants.
+    /// </summary>
+    public class MatchmakingMembers : List<string>
+    {
+    }
+
+    /// <summary>
+    /// In this matchmaking controller, we are storing the temporary list of members who are joining the matchmaking in SFKV.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class MatchmakingController : Controller
+    {
+        private const string _matchmakingStoreKey = "zurazura.matchmaking";
+        
+        [HttpGet]
+        public async Task<MatchmakingMembers> ListMembers()
+        {
+            MatchmakingEventSource.Current.ServiceRequestStart(nameof(ListMembers), DateTime.Now.ToString("o"), null);
+
+            try
+            {
+                return await GetMatchmakingMembersFromStoreAsync();
+            }
+            catch (Exception ex)
+            {
+                MatchmakingEventSource.Current.ServiceRequestError(nameof(ListMembers), ex.ToString());
+
+                throw;
+            }
+            finally
+            {
+                MatchmakingEventSource.Current.ServiceRequestStop(nameof(ListMembers), DateTime.Now.ToString("o"));
+            }
+        }
+        
+        [HttpPost]
+        public async Task Join([FromForm]string name)
+        {
+            MatchmakingEventSource.Current.ServiceRequestStart(nameof(Join), DateTime.Now.ToString("o"), JsonConvert.SerializeObject(new { name }));
+
+            try
+            {
+                var currentMembers = await GetMatchmakingMembersFromStoreAsync();
+
+                currentMembers.Add(name);
+                await SetMatchmakingMembersToStoreAsync(currentMembers);
+            }
+            catch (Exception ex)
+            {
+                MatchmakingEventSource.Current.ServiceRequestError(nameof(Join), ex.ToString());
+
+                throw;
+            }
+            finally
+            {
+                MatchmakingEventSource.Current.ServiceRequestStop(nameof(Join), DateTime.Now.ToString("o"));
+            }
+        }
+
+        [HttpDelete("{name}")]
+        public async Task Leave(string name)
+        {
+            MatchmakingEventSource.Current.ServiceRequestStart(nameof(Leave), DateTime.Now.ToString("o"), JsonConvert.SerializeObject(new { name }));
+
+            try
+            {
+                var currentMembers = await GetMatchmakingMembersFromStoreAsync();
+
+                currentMembers.Remove(name);
+                await SetMatchmakingMembersToStoreAsync(currentMembers);
+            }
+            catch (Exception ex)
+            {
+                MatchmakingEventSource.Current.ServiceRequestError(nameof(Leave), ex.ToString());
+
+                throw;
+            }
+            finally
+            {
+                MatchmakingEventSource.Current.ServiceRequestStop(nameof(Leave), DateTime.Now.ToString("o"));
+            }
+        }
+
+        private async Task<MatchmakingMembers> GetMatchmakingMembersFromStoreAsync()
+        {
+            // The matchmaking controller use random secondary replicas for read operation.
+            var storeProxy = CreateStoreProxy(TargetReplicaSelector.RandomSecondaryReplica);
+
+            var result = await storeProxy.StringGet(_matchmakingStoreKey);
+
+            if (result != null)
+            {
+                return JsonConvert.DeserializeObject<MatchmakingMembers>(result);
+            }
+            else
+            {
+                return new MatchmakingMembers();
+            }
+        }
+
+        private Task SetMatchmakingMembersToStoreAsync(MatchmakingMembers members)
+        {
+            var storeProxy = CreateStoreProxy(TargetReplicaSelector.PrimaryReplica);
+            return storeProxy.StringSet(_matchmakingStoreKey, JsonConvert.SerializeObject(members));
+        }
+        
+        private IStore CreateStoreProxy(TargetReplicaSelector targetReplicaSelector)
+        {
+            // The matchmaking service only store the matchmaking members in a single partition.
+            return ServiceProxy.Create<IStore>(
+                new Uri("fabric:/SFKV/SFKV.Store"),
+                new ServicePartitionKey(0),
+                targetReplicaSelector);
+        }
+    }
+}
---
+++ Examples/ZuraZura/ZuraZura.MatchmakingService/MatchmakingEventSource.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Fabric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Newtonsoft.Json;
+
+namespace ZuraZura.MatchmakingService
+{
+    [EventSource(Name = "ZuraZura.MatchmakingService")]
+    internal sealed class MatchmakingEventSource : EventSource
+    {
+        public static readonly MatchmakingEventSource Current = new MatchmakingEventSource();
+
+        #region Singleton Construct
+        static MatchmakingEventSource()
+        {
+            // A workaround for the problem where ETW activities do not get tracked until Tasks infrastructure is initialized.
+            // This problem will be fixed in .NET Framework 4.6.2.
+            Task.Run(() => { });
+        }
+
+        // Instance constructor is private to enforce singleton semantics
+        private MatchmakingEventSource() : base() { } 
+        #endregion
+
+        public static class Keywords
+        {
+            public const EventKeywords Requests = (EventKeywords)0x1L;
+            public const EventKeywords ServiceInitialization = (EventKeywords)0x2L;
+        }
+
+        [NonEvent]
+        public void Message(string message, params object[] args)
+        {
+            if (this.IsEnabled())
+            {
+                string finalMessage = string.Format(message, args);
+                Message(finalMessage);
+            }
+        }
+
+        private const int MessageEventId = 1;
+        [Event(MessageEventId, Level = EventLevel.Informational, Message = "{0}")]
+        public void Message(string message)
+        {
+            if (this.IsEnabled())
+            {
+                WriteEvent(MessageEventId, message);
+            }
+        }
+
+        [NonEvent]
+        public void ServiceMessage(ServiceContext serviceContext, string message, params object[] args)
+        {
+            if (this.IsEnabled())
+            {
+
+                string finalMessage = string.Format(message, args);
+                ServiceMessage(
+                    serviceContext.ServiceName.ToString(),
+                    serviceContext.ServiceTypeName,
+                    GetReplicaOrInstanceId(serviceContext),
+                    serviceContext.PartitionId,
+                    serviceContext.CodePackageActivationContext.ApplicationName,
+                    serviceContext.CodePackageActivationContext.ApplicationTypeName,
+                    serviceContext.NodeContext.NodeName,
+                    finalMessage);
+            }
+        }
+
+        // For very high-frequency events it might be advantageous to raise events using WriteEventCore API.
+        // This results in more efficient parameter handling, but requires explicit allocation of EventData structure and unsafe code.
+        // To enable this code path, define UNSAFE conditional compilation symbol and turn on unsafe code support in project properties.
+        private const int ServiceMessageEventId = 2;
+        [Event(ServiceMessageEventId, Level = EventLevel.Informational, Message = "{7}")]
+        private
+#if UNSAFE
+        unsafe
+#endif
+        void ServiceMessage(
+            string serviceName,
+            string serviceTypeName,
+            long replicaOrInstanceId,
+            Guid partitionId,
+            string applicationName,
+            string applicationTypeName,
+            string nodeName,
+            string message)
+        {
+#if !UNSAFE
+            WriteEvent(ServiceMessageEventId, serviceName, serviceTypeName, replicaOrInstanceId, partitionId, applicationName, applicationTypeName, nodeName, message);
+#else
+            const int numArgs = 8;
+            fixed (char* pServiceName = serviceName, pServiceTypeName = serviceTypeName, pApplicationName = applicationName, pApplicationTypeName = applicationTypeName, pNodeName = nodeName, pMessage = message)
+            {
+                EventData* eventData = stackalloc EventData[numArgs];
+                eventData[0] = new EventData { DataPointer = (IntPtr) pServiceName, Size = SizeInBytes(serviceName) };
+                eventData[1] = new EventData { DataPointer = (IntPtr) pServiceTypeName, Size = SizeInBytes(serviceTypeName) };
+                eventData[2] = new EventData { DataPointer = (IntPtr) (&replicaOrInstanceId), Size = sizeof(long) };
+                eventData[3] = new EventData { DataPointer = (IntPtr) (&partitionId), Size = sizeof(Guid) };
+                eventData[4] = new EventData { DataPointer = (IntPtr) pApplicationName, Size = SizeInBytes(applicationName) };
+                eventData[5] = new EventData { DataPointer = (IntPtr) pApplicationTypeName, Size = SizeInBytes(applicationTypeName) };
+                eventData[6] = new EventData { DataPointer = (IntPtr) pNodeName, Size = SizeInBytes(nodeName) };
+                eventData[7] = new EventData { DataPointer = (IntPtr) pMessage, Size = SizeInBytes(message) };
+
+                WriteEventCore(ServiceMessageEventId, numArgs, eventData);
+            }
+#endif
+        }
+
+        private const int ServiceTypeRegisteredEventId = 3;
+        [Event(ServiceTypeRegisteredEventId, Level = EventLevel.Informational, Message = "Service host process {0} registered service type {1}", Keywords = Keywords.ServiceInitialization)]
+        public void ServiceTypeRegistered(int hostProcessId, string serviceType)
+        {
+            WriteEvent(ServiceTypeRegisteredEventId, hostProcessId, serviceType);
+        }
+
+        private const int ServiceHostInitializationFailedEventId = 4;
+        [Event(ServiceHostInitializationFailedEventId, Level = EventLevel.Error, Message = "Service host initialization failed", Keywords = Keywords.ServiceInitialization)]
+        public void ServiceHostInitializationFailed(string exception)
+        {
+            WriteEvent(ServiceHostInitializationFailedEventId, exception);
+        }
+        
+        private const int ServiceRequestStartEventId = 5;
+        [Event(ServiceRequestStartEventId, Level = EventLevel.Informational, Message = "Service request '{0}' started at '{1}'", Keywords = Keywords.Requests)]
+        public void ServiceRequestStart(string requestTypeName, string currentTime, string arguments)
+        {
+            WriteEvent(ServiceRequestStartEventId, requestTypeName, currentTime, arguments);
+        }
+
+        private const int ServiceRequestStopEventId = 6;
+        [Event(ServiceRequestStopEventId, Level = EventLevel.Informational, Message = "Service request '{0}' finished at '{1}'", Keywords = Keywords.Requests)]
+        public void ServiceRequestStop(string requestTypeName, string currentTime)
+        {
+            WriteEvent(ServiceRequestStopEventId, requestTypeName, currentTime);
+        }
+
+        private const int ServiceRequestErrorEventId = 7;
+        [Event(ServiceRequestErrorEventId, Level = EventLevel.Error, Message = "Service request '{0}' encounters one or more errors.", Keywords = Keywords.Requests)]
+        public void ServiceRequestError(string requestTypeName, string exception)
+        {
+            WriteEvent(ServiceRequestErrorEventId, requestTypeName, exception);
+        }
+
+        #region Private methods
+        private static long GetReplicaOrInstanceId(ServiceContext context)
+        {
+            StatelessServiceContext stateless = context as StatelessServiceContext;
+            if (stateless != null)
+            {
+                return stateless.InstanceId;
+            }
+
+            StatefulServiceContext stateful = context as StatefulServiceContext;
+            if (stateful != null)
+            {
+                return stateful.ReplicaId;
+            }
+
+            throw new NotSupportedException("Context type not supported.");
+        }
+#if UNSAFE
+        private int SizeInBytes(string s)
+        {
+            if (s == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return (s.Length + 1) * sizeof(char);
+            }
+        }
+#endif
+        #endregion
+    }
+}
